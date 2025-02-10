0834cdbc55541ec6c9681dd6d39821f71cc7b155
ben-forbes-1 <b.forbes@student.uq.edu.au>
2025-02-10 16:50:34 +1000

    clean up

--- YFinanceDownloader.py
+++ YFinanceDownloader.py
@@ -15,11 +15,13 @@ class YFinanceDownloader:
         if not expirations:
             raise ValueError("No options available for this stock")
 
+        # Iterate through expirations and concat to all_data
         for expiry in expirations:
             options_chain = stock.option_chain(expiry)
             calls = options_chain.calls
             puts = options_chain.puts
 
+            # Filter out options with no volume / open interest / bid / ask
             calls = calls[(calls['volume'] > 0) & (calls['openInterest'] > 0) & (calls['bid'] > 0) & (calls['ask'] > 0)]
             puts = puts[(puts['volume'] > 0) & (puts['openInterest'] > 0) & (puts['bid'] > 0) & (puts['ask'] > 0)]
 
@@ -27,13 +29,14 @@ class YFinanceDownloader:
             options['Expiration Date'] = pd.to_datetime(expiry)
             all_data = pd.concat([all_data, options], ignore_index=True)
 
+        # Add index spot to all_data
         all_data['Index Spot'] = stock.history(period="1d").iloc[-1]["Close"]
         all_data = all_data.sort_values(by=['Expiration Date', 'strike'])
 
         if len(all_data.index) != 0:
             combined_csv_path = "spx_options_combined.csv"
             if os.path.exists(combined_csv_path):
-                os.remove(combined_csv_path)
+                os.remove(combined_csv_path) # Remove existing file
             all_data.to_csv(combined_csv_path, index=False)
             print(f"Data saved to {combined_csv_path}")
         else:
